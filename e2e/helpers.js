7677b9dacc4d2476a72b9cff1aa8806bddef8a12
Saul Crawley <scrawley2@sheffield.ac.uk>
2025-11-15 23:43:39 +0000

    Lab 6

---
+++ e2e/helpers.js
@@ -0,0 +1,12 @@
+import { expect } from '@playwright/test';
+
+export async function signup(page, username, password) {
+    await page.goto('http://localhost:5173/signup');
+
+    await page.fill('input[id="username"]', username);
+    await page.fill('input[id="password"]', password);
+
+    await page.click('button[type="submit"]');
+
+    await expect(page.getByRole('button', { name: 'Logout' })).toBeVisible()
+}
